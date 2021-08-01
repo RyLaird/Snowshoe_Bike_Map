73a808a85745b44bcc5dcd0e022463793e9bf10d
RyLaird <46271384+RyLaird@users.noreply.github.com>
2021-08-01 08:51:10 -0500

    updating routes

--- Snowshoe/snowshoe_trails/routes/index.js
+++ Snowshoe/snowshoe_trails/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+router.use(express.urlencoded({ extended: true })); // <--- middleware configuration
+
 
 const {Client, Query } = require('pg')
 
@@ -10,7 +12,7 @@ var host = "localhost:5432"
 var database = "snowshoe"
 var conString = "postgres://"+username+":"+password+"@"+host+"/"+database;
 
-var testQuery = "SELECT row_to_json(fc) FROM (SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry,row_to_json((gid, name, difficulty, descriptio)) As properties FROM snowshoetrails As lg) As f) As fc"
+var testQuery = "SELECT row_to_json(fc) FROM (SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry,row_to_json((gid, name, difficulty, descriptio, closed)) As properties FROM snowshoetrails As lg) As f) As fc"
 
 
 /* GET home page. */
@@ -64,13 +66,43 @@ router.get('/employee', function(req, res) {
   //Pass result to employee page
   query.on("end", function (result) {
     var data =result.rows[0].row_to_json
+    console.log(result)
     res.render('employee', {
       title: "Employee Map",
-      jsonData: data
+      jsonData: data,
+      trails: data['features']
     });
   });
 });
 
+const  { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost:5432",
+  database: "snowshoe",
+  password: "admin",
+  port: 5432
+});
+
+router.post('/updateStatus', function(req,res) {
+  // var client = new Client(conString);
+  // client.connect()
+  console.log(req.body)
+  const trail = [req.body.status, req.body.trail]
+  const statusQuery = "UPDATE snowshoetrails SET closed = $1 WHERE (name = $2)";
+  pool.query(statusQuery, trail, function(err, result) {
+    if (err) {
+      console.error(err)
+    }
+    else {
+      alert('Your update was successful')
+      res.redirect('/employee')
+    }
+
+    // res.status(200).send({ message: "Product Updated Successfully!" });
+  });
+});
 
 
 
